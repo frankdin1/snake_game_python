b9ebdfb1a87bf925dd4b7282c4a941d1af77aba6
frankdin <frankdin@yahoo.com>
2024-04-14 13:53:43 -0400

    1. Modified code that initialised snake segments by changing the initial order of snake segments'. 2. Turned on screen tracer and screen update to improve illusion of snake segments moving as one body. 3. Imported time module to slow down snake movement. 4. Added code to enable snake segments to 'turn' as one body after reaching a certain point on the screen.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from turtle import Turtle, Screen
-
-initial_x_cor = 0.0
+import time
+initial_x_cor = 0
 snake_body = []  # array that comprises different snake segments
 # creating individual segments of the snake body and 'merging' them
 for _ in range(3):
@@ -10,24 +10,25 @@ for _ in range(3):
     new_turtle.goto(initial_x_cor, 0)
     snake_body.append(new_turtle)  # adding each segment to the array to form a 'solid' body
     print(new_turtle.position())
-    initial_x_cor += 20
+    initial_x_cor -= 20
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(3)
 
 game_is_on = True
+
 # moving segments of snake body as single entity
 while game_is_on:
-    moves = 0
+    screen.update()
+    time.sleep(1)
     for segment in snake_body:
-        segment.speed("slow")
         segment.forward(20)
-        print(segment.xcor())
-        # stopping the snake movement once the 'head' segment of the body reaches position (200, 0)
+        # affecting the snake movement once the 'head' segment of the body reaches position (200, 0)
         if segment.xcor() >= 200.0:
-            #segment.setheading(90)
-            game_is_on = False
+            segment.setheading(90)  # changes the snakes direction
+            #game_is_on = False # stops the movement of the snake
 
 screen.exitonclick()
