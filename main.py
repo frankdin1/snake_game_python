f5770901ba516031eac39bb36870cc509fdeffff
frankdin <frankdin@yahoo.com>
2024-04-14 14:56:01 -0400

    1. Changed the number in tracer back to 3. 2. Modified line in while loop to end game if snake reached the edge of the screen.

--- main.py
+++ main.py
@@ -16,19 +16,28 @@ screen = Screen()
 screen.setup(width=300, height=300)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-screen.tracer(2)
-print(screen.window_width())
+screen.tracer(3)
+print(screen.window_width(), screen.window_height())
 game_is_on = True
+
+
+def move_left():
+    segment.setheading(90)
+
 
 # moving segments of snake body as single entity
 while game_is_on:
     screen.update()
     time.sleep(1)
     for segment in snake_body:
-        segment.forward(20)
+        screen.onkey(key="s", fun=move_left)
+        segment.backward(20)
         print(segment.pos())
         # affecting the snake movement once the 'head' segment of the body reaches position (200, 0)
-        if segment.xcor() >= screen.window_width() or segment.ycor() >= screen.window_height():
+        if segment.xcor() >= screen.window_width() / 2 or \
+                segment.ycor() >= screen.window_height() / 2 or \
+                segment.xcor() <= -screen.window_width() / 2 or \
+                segment.ycor() <= -screen.window_height() / 2:
             #segment.setheading(90)  # changes the snakes direction
             game_is_on = False # stops the movement of the snake
 
