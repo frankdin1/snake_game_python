bbbcfb9dfbf6e4bad0dd150892ea66239f7a8f5c
frankdin <frankdin@yahoo.com>
2024-04-14 12:57:51 -0400

    Used a for-in loop to create the initial 'snake' body.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from turtle import Turtle, Screen
 
+initial_x_cor = 0.0
+for _ in range(3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.pu()
+    new_turtle.goto(initial_x_cor, 0)
+    print(new_turtle.position())
+    initial_x_cor += 20
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
 
 
 
