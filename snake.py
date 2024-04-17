aa656c331733d6724e11fc072bd13a3b73b17db5
frankdin <frankdin@yahoo.com>
2024-04-16 21:53:20 -0400

    1. Removed the @staticmethod identifier from the move_snake method. 2. Added comments to the move_snake method.

--- snake.py
+++ snake.py
@@ -9,10 +9,11 @@ class Snake:
     def __init__(self):
         self.screen = screen
         self.time = time
-        self.snake_body = []
+        self.snake_body = []  # array that comprises different snake segments
         self.initial_x_cor = 0
         self.create_snake()
 
+    # creating individual segments of the snake body and 'merging' them
     def create_snake(self):
         for _ in range(3):
             new_turtle = Turtle(shape="square")
@@ -23,12 +24,15 @@ class Snake:
             print(new_turtle.position())
             self.initial_x_cor -= 20
 
-    @staticmethod
     def move_snake(self):
         screen.update()
         time.sleep(1)
+        # starting with the last segment...
         for segment_number in range(len(self.snake_body) - 1, 0, -1):
+            # ...collect the x and y coordinates of the next segment
             new_xcor = self.snake_body[segment_number - 1].xcor()
             new_ycor = self.snake_body[segment_number - 1].ycor()
+
+            # move the current segment to the position of the next segment
             self.snake_body[segment_number].goto(new_xcor, new_ycor)
         self.snake_body[0].forward(20)
