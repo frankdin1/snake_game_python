d3e5fe24ad1ae6407789a1629296d3bd242447c1
frankdin <frankdin@yahoo.com>
2024-04-23 09:52:13 -0400

    Changed the amount by which the numbers for x position and y position would increment.

---
+++ dot.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+import random
+
+
+class Dot:
+    def __init__(self, screen_width, screen_height):
+        self.dot = Turtle()
+        self.x_pos = range(round(-screen_width / 2), round(screen_width / 2), 20)
+        self.y_pos = range(round(-screen_height / 2), round(screen_height / 2), 20)
+        self.create_dot()
+
+    def create_dot(self):
+        self.dot.shape("square")
+        self.dot.color("blue")
+        self.dot.pu()
+        self.dot.goto(random.choice(self.x_pos), random.choice(self.y_pos))
