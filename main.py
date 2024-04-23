188dfa7b604d5cbd83f5504cec03a54917903402
frankdin <frankdin@yahoo.com>
2024-04-23 09:53:36 -0400

    Changed the dimensions of the screen window to even numbers to make it easier to match the position of the dot to the position of the snake's head.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dot import Dot
 
 snake = Snake()
 screen = Screen()
-screen.setup(width=300, height=300)
+screen.setup(width=400, height=400)
 dot = Dot(screen.window_width(), screen.window_height())
 screen.bgcolor("black")
 screen.title("My Snake Game")
@@ -22,12 +22,12 @@ screen.onkey(screen.bye, "Escape")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.2)
     snake.move_snake()
     print(f"dot position\tsnake position")
     print(f"{dot.dot.position()}\t{snake.snake_body[0].position()}")
-    # if snake.snake_body[0].position() == dot.dot.position():
-    #     print("contact")
+    if snake.snake_body[0].position() == dot.dot.position():
+        print("contact")
 
 
 screen.exitonclick()
