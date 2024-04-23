9c8b718f7e8ec5b74571d1b28da22e21e7fdd501
frankdin <frankdin@yahoo.com>
2024-04-23 10:13:54 -0400

    Moved the line of code that creates a new dot object into the function  'create_dot'. Added the line 'self.dot = None' to the 'intializer'.

--- dot.py
+++ dot.py
@@ -4,12 +4,13 @@ import random
 
 class Dot:
     def __init__(self, screen_width, screen_height):
-        self.dot = Turtle()
+        self.dot = None
         self.x_pos = range(round(-screen_width / 2), round(screen_width / 2), 20)
         self.y_pos = range(round(-screen_height / 2), round(screen_height / 2), 20)
         self.create_dot()
 
     def create_dot(self):
+        self.dot = Turtle()
         self.dot.shape("square")
         self.dot.color("blue")
         self.dot.pu()
